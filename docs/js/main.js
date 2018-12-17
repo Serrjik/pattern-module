c9a79591cf228dc0e98721e8f37529dfad843896
Serrjik <uac.sarge@gmail.com>
2018-12-17 14:18:41 +0300

    Homework ready

---
+++ docs/js/main.js
@@ -0,0 +1,50 @@
+// Запишем анонимную самовызывающуюся ф-ю в переменную counter
+
+var counter = (function () {
+	// Приватные переменные (начинаются с нижнего подчёркивания)
+	var __number;
+
+	// Приватные методы
+	var _checkPositive = function(positiveNumber){
+		if ( positiveNumber < 0 ) {
+			return false;
+		} else {
+			return true;
+		}
+	}
+
+	var setValue = function(positiveNumber){
+		if ( _checkPositive(positiveNumber) ) {
+			_number = positiveNumber;
+		} else {
+			console.log("Неверное значение. Должно быть больше или равно 0.")
+		}
+	}
+
+	var increaseCounter = function(){
+		_number++;
+	}
+
+	var decreaseCounter = function(){
+		_number--;
+	}
+
+	var printCounter = function(){
+		console.log(_number);
+	}
+
+	// Чтобы иметь доступ к внутренним переменным
+	// их надо вернуть через return
+	return {
+		setValue,
+		increaseCounter,
+		decreaseCounter,
+		printCounter
+	}
+}());
+
+counter.setValue(5);
+counter.printCounter(); // 7
+counter.increaseCounter(); // 6
+counter.increaseCounter(); // 7
+counter.printCounter(); // 7
